9bac6e0c76edde3b631bbb2801b8f1857e19f6cb
Austeja <austeja.sumsyte@gmail.com>
2020-10-03 13:50:10 +0300

    - Adding missing files

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// assets/js/app.js
+import Vue from 'vue';
+import App from './components/App'
+/**
+ * Create a fresh Vue Application instance
+ */
+new Vue({
+    el: '#app',
+    components: {App}
+});
