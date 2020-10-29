b70fe2e07968f4a977a77de267b54cada6809344
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-10-29 22:38:57 +0200

    fix: Fixed migrations

--- migrations/Version20201029203756.php
+++ migrations/Version20201029203756.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201029202945 extends AbstractMigration
+final class Version20201029203756 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,10 +20,7 @@ final class Version20201029202945 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE sound_package DROP FOREIGN KEY FK_DC5AE6EA41AE7C1A');
-        $this->addSql('DROP INDEX IDX_DC5AE6EA41AE7C1A ON sound_package');
-        $this->addSql('ALTER TABLE sound_package CHANGE sound_package_id category_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE sound_package ADD category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE sound_package ADD CONSTRAINT FK_DC5AE6EA12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('CREATE INDEX IDX_DC5AE6EA12469DE2 ON sound_package (category_id)');
     }
@@ -32,10 +29,7 @@ final class Version20201029202945 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sound_package DROP FOREIGN KEY FK_DC5AE6EA12469DE2');
-        $this->addSql('DROP TABLE category');
         $this->addSql('DROP INDEX IDX_DC5AE6EA12469DE2 ON sound_package');
-        $this->addSql('ALTER TABLE sound_package CHANGE category_id sound_package_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE sound_package ADD CONSTRAINT FK_DC5AE6EA41AE7C1A FOREIGN KEY (sound_package_id) REFERENCES sound_package (id)');
-        $this->addSql('CREATE INDEX IDX_DC5AE6EA41AE7C1A ON sound_package (sound_package_id)');
+        $this->addSql('ALTER TABLE sound_package DROP category_id');
     }
 }
