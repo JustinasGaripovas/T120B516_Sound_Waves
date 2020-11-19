cdffef0af713c1d30ee7def6d660bac49e414526
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-11-19 23:49:36 +0200

    fix: File Upload logic

--- src/Controller/SoundFileController.php
+++ src/Controller/SoundFileController.php
@@ -6,9 +6,11 @@ use App\Entity\SoundFile;
 use App\Form\SoundFileType;
 use App\Repository\SoundFileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/sound/file")
@@ -28,13 +30,30 @@ class SoundFileController extends AbstractController
     /**
      * @Route("/new", name="sound_file_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $soundFile = new SoundFile();
         $form = $this->createForm(SoundFileType::class, $soundFile);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $brochureFile = $form->get('brochure')->getData();
+
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $brochureFile->guessExtension();
+
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('upload-directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                }
+                $soundFile->setFilename($newFilename);
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($soundFile);
             $entityManager->flush();
--- src/Form/SoundFileType.php
+++ src/Form/SoundFileType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\SoundFile;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class SoundFileType extends AbstractType
 {
@@ -16,6 +18,29 @@ class SoundFileType extends AbstractType
             ->add('createdAt')
             ->add('updatedAt')
             ->add('soundPackage')
+            ->add('brochure', FileType::class, [
+                'label' => 'Music (MP3 file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                             'maxSize' => '20M',
+                             'mimeTypes' => [
+                                 'audio/mpeg',
+                                 'audio/mp3',
+                             ],
+                             'mimeTypesMessage' => 'Please upload a valid MP3 file',
+                         ])
+                ],
+            ])
         ;
     }
 
